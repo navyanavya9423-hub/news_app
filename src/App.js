1fe6c06e5fcd615a4b8d8f64845dcc7532fb70e2
navya <navyanavya9423@gmail.com>
2026-02-16 20:56:03 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <News />
+    </>
+  );
+}
+
+export default App;
