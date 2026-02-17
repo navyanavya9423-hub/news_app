4961370bbe0e45eb11f9beabb9c86a70470c1515
navya <navyanavya9423@gmail.com>
2026-02-17 13:02:25 +0530

    new

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,56 +1,69 @@
-import React from "react";
+import React from 'react'
+import Newsitem from './Newsitem'
 
-export default function Navbar() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
+export default class News extends React.Component {
 
-        {/* Brand */}
-        <a className="navbar-brand" href="/">Navbar</a>
+  constructor(props){
+    super(props)
+    console.log("hello i am a constructor from news component")
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1
+    }
+  }
+  fetchNews = async (page = 1) => {
+    this.setState({ loading: true });
+    const pageSize = 20;
+    const url = `https://newsapi.org/v2/everything?q=tesla&from=2026-01-16&sortBy=publishedAt&page=${page}&pageSize=${pageSize}&apiKey=70442fe3aec54043852b5299650a22f1`;
 
-        {/* Toggle Button */}
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+    try {
+      const data = await fetch(url);
+      const parsedData = await data.json();
+      this.setState({ articles: parsedData.articles || [], page, loading: false });
+    } catch (err) {
+      console.error(err);
+      this.setState({ loading: false });
+    }
+  }
 
-        {/* Menu */}
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
+  async componentDidMount(){
+    this.fetchNews(1);
+  }
 
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">Home</a>
-            </li>
+  previousPage = async () => {
+    const prevPage = Math.max(1, this.state.page - 1);
+    await this.fetchNews(prevPage);
+  }
 
-            <li className="nav-item">
-              <a className="nav-link" href="/">Features</a>
-            </li>
+  nextPage = async () => {
+    const nextPage = this.state.page + 1;
+    await this.fetchNews(nextPage);
 
-            <li className="nav-item">
-              <a className="nav-link" href="/">Pricing</a>
-            </li>
+  }
 
-            <li className="nav-item">
-              <a
-                className="nav-link disabled"
-                href="/"
-                tabIndex="-1"
-                aria-disabled="true"
-              >
-                Disabled
-              </a>
-            </li>
-
-          </ul>
-        </div>
+  render() {
+    return (
+      <div className="container my-3">
+        <h1 className="text-center"> My News Top Headlines</h1>
+        <div className="row">
+          {this.state.articles.map(element => {
+            return (
+              <div className="col-md-4" key={element.url}>
+                <Newsitem
+                  title={element.title}
+                  description={element.description}
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            )
+          })}
+        </div><div className="d-flex justify-content-center gap-3 mt-5">
+          <button disabled={this.state.page <= 1} type="button" onClick={this.previousPage} className="btn btn-dark">&larr; Previous</button>
+          <button type="button" onClick={this.nextPage} className="btn btn-dark">Next &rarr;</button>
       </div>
-    </nav>
-  );
+      </div>
+    )
+  }
 }
