f370b15fb52a605294d52731a163c55bcc42aa50
navya <navyanavya9423@gmail.com>
2026-02-17 12:01:40 +0530

    new

--- src/components/News.js
+++ src/components/News.js
@@ -6,133 +6,63 @@ export default class News extends React.Component {
   constructor(props){
     super(props)
     console.log("hello i am a constructor from news component")
-
     this.state = {
       articles: [],
       loading: false,
-      page: 1,
-      totalResults: 0
+      page: 1
+    }
+  }
+  fetchNews = async (page = 1) => {
+    this.setState({ loading: true });
+    const pageSize = 20;
+    const url = `https://newsapi.org/v2/everything?q=tesla&from=2026-01-16&sortBy=publishedAt&page=${page}&pageSize=${pageSize}&apiKey=70442fe3aec54043852b5299650a22f1`;
+
+    try {
+      const data = await fetch(url);
+      const parsedData = await data.json();
+      this.setState({ articles: parsedData.articles || [], page, loading: false });
+    } catch (err) {
+      console.error(err);
+      this.setState({ loading: false });
     }
   }
 
   async componentDidMount(){
-    console.log("cdm");
-
-    const url = `https://newsapi.org/v2/everything?q=tesla&from=2026-01-16&sortBy=publishedAt&page=1&pageSize=9&apiKey=2fbde6c4f937469f8a851608acaddce3`;
-
-    try {
-      const data = await fetch(url);
-      const parsedData = await data.json();
-      console.log(parsedData);
-
-      this.setState({
-        articles: parsedData.articles || [],
-        totalResults: parsedData.totalResults || 0
-      });
-
-    } catch (err) {
-      console.error(err);
-    }
+    this.fetchNews(1);
   }
 
-
-  previouspage = async () => {
-
-    if (this.state.page <= 1) return;
-
-    const newPage = this.state.page - 1;
-
-    console.log("previous - fetching page", newPage);
-
-    const url = `https://newsapi.org/v2/everything?q=tesla&from=2026-01-16&sortBy=publishedAt&page=${newPage}&pageSize=9&apiKey=2fbde6c4f937469f8a851608acaddce3`;
-
-    try {
-      const data = await fetch(url);
-      const parsedData = await data.json();
-
-      this.setState({
-        page: newPage,
-        articles: parsedData.articles || []
-      });
-
-    } catch (err) {
-      console.error(err);
-    }
-  } 
-
-
-  nextpage = async () => {
-
-    const newPage = this.state.page + 1;
-
-    console.log("next - fetching page", newPage);
-
-    const url = `https://newsapi.org/v2/everything?q=tesla&from=2026-01-16&sortBy=publishedAt&page=${newPage}&pageSize=9&apiKey=2fbde6c4f937469f8a851608acaddce3`;
-
-    try {
-      const data = await fetch(url);
-      const parsedData = await data.json();
-
-      this.setState({
-        page: newPage,
-        articles: parsedData.articles || []
-      });
-
-    } catch (err) {
-      console.error(err);
-    }
+  previousPage = async () => {
+    const prevPage = Math.max(1, this.state.page - 1);
+    await this.fetchNews(prevPage);
   }
 
+  nextPage = async () => {
+    const nextPage = this.state.page + 1;
+    await this.fetchNews(nextPage);
 
+  }
 
   render() {
     return (
       <div className="container my-3">
-
         <h1 className="text-center"> My News Top Headlines</h1>
-
         <div className="row">
-
-          {this.state.articles.map((element) => {
-
+          {this.state.articles.map(element => {
             return (
               <div className="col-md-4" key={element.url}>
-
                 <Newsitem
-                  title={element.title ? element.title.slice(0, 50) : ""}
-                  description={element.description ? element.description.slice(0, 88) : ""}
+                  title={element.title}
+                  description={element.description}
                   imageUrl={element.urlToImage}
                   newsUrl={element.url}
                 />
-
               </div>
             )
           })}
-
-        </div>
-
-
-        <div className="container d-flex justify-content-between">
-
-          <button
-            disabled={this.state.page <= 1}
-            type="button"
-            className="btn btn-dark"
-            onClick={this.previouspage}
-          >
-            &larr; Previous
-          </button>
-
-          <button
-            type="button"
-            className="btn btn-dark"
-            onClick={this.nextpage}
-          >
-            Next &rarr;
-          </button>
-
-        </div>
-
+        </div><div className="d-flex justify-content-center gap-3 mt-5">
+          <button disabled={this.state.page <= 1} type="button" onClick={this.previousPage} className="btn btn-dark">&larr; Previous</button>
+          <button type="button" onClick={this.nextPage} className="btn btn-dark">Next &rarr;</button>
+      </div>
       </div>
     )
   }
